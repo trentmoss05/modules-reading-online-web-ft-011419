44d3ba7e4c872205e2dd8590f87f03132ae69626
Trent Moss <trentmoss05@gmail.com>
2019-01-30 16:13:55 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,8 +4,8 @@ require_relative './fancy_dance.rb'
 
 class Dancer
 
-  include Dance
-  extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
 
   attr_accessor :name
 
